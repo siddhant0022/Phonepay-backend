04003152220ef4e4567e0a2d8526092963700192
siddhant0022 <148846164+siddhant0022@users.noreply.github.com>
2026-04-19 15:20:51 +0530

    Auth done

--- src/controllers/authContollers.js
+++ src/controllers/authContollers.js
@@ -92,13 +92,17 @@ const LoginUser = async (req, res) => {
 
 const LogoutUser = async (req, res) => {
   try{
-    res.clearCokkie("token");
+    res.clearCookie("token");
     res.status(200).json({
       message: "User Logged out successfully"
     })
   } catch(err){
+    res.status(500).json({
+      message: "Error in Logout the user",
+      error: err.message
+    })
 
   }
 }
 
-module.exports = {registerUser, LoginUser};
+module.exports = {registerUser, LoginUser, LogoutUser};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const Router = require('express');
 const Authrouter = Router();
-const {registerUser, LoginUser} = require('../controllers/authContollers');
+const {registerUser, LoginUser, LogoutUser} = require('../controllers/authContollers');
 
 Authrouter.post('/register', registerUser);
 Authrouter.post('/login', LoginUser);
+Authrouter.get('/logout', LogoutUser );
 
 module.exports = Authrouter;
